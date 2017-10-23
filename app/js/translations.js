f4006f1e82cf59f570ae02d87d30820dbe58bec7
mkruneva <marta.kruneva@gmail.com>
2017-10-23 11:06:54 +0100

    tams - look biography

--- app/js/translations.js
+++ app/js/translations.js
@@ -60,7 +60,8 @@ function Translations($translateProvider) {
       PAST_EVENTS: "Past Events",
       NO_FUTURE_EVENTS: "No upcoming events.",
       SEO_DESCRIPTION_EMI: "The Energy Management Institute is an non-governmental organization, its mission is to contribute to the sustainable development of the Bulgarian energy sector and its integration to the European Union",
-      SEO_KEYWORDS: 'EMI, Energy Management Institute, ЕМИ, Институт Енергиен Мениджмънт, eurelectric Bulgaria, енергетика, energy, research, energy trainings, енергийна полиика, обучения, events, енергийна инфраструктура '
+      SEO_KEYWORDS: 'EMI, Energy Management Institute, ЕМИ, Институт Енергиен Мениджмънт, eurelectric Bulgaria, енергетика, energy, research, energy trainings, енергийна полиика, обучения, events, енергийна инфраструктура ',
+      BIO: 'Biography'
     })
     .translations('bg', {
       HOME: 'Начало',
@@ -116,7 +117,8 @@ function Translations($translateProvider) {
       PAST_EVENTS: "Отминали събития",
       NO_FUTURE_EVENTS: "Няма предстоящи събития.",
       SEO_DESCRIPTION_EMI: 'Институтът за Енергиен Мениджмънт (ЕMI) е неправителствена организация, мисията й е да допринася за устойчивото развитие на българската енергетика и интеграцията й към тази на Европейския Съюз.',
-      SEO_KEYWORDS: 'ЕМИ, Институт Енергиен Мениджмънт, Енергиен Мениджмънт, енергетика, поликика, енергийна полиика, обучения, събития, цени ток, газ, въглища, парно, енергийна инфраструктура '
+      SEO_KEYWORDS: 'ЕМИ, Институт Енергиен Мениджмънт, Енергиен Мениджмънт, енергетика, поликика, енергийна полиика, обучения, събития, цени ток, газ, въглища, парно, енергийна инфраструктура ',
+      BIO: 'Биография'
     });
   $translateProvider.preferredLanguage('bg');
 }
