c341bf48a7f7c70a5a3a2698dff7cb06863548fa
mkruneva <marta.kruneva@gmail.com>
2017-11-17 20:32:49 +0000

    delete and restore for partners - looks like the partner is deleted only from the list and not the data base

--- app/js/controllers/admin-partners.js
+++ app/js/controllers/admin-partners.js
@@ -3,7 +3,7 @@
 var controllersModule = require('./_index');
 var _ = require('lazy.js');
 
-function AdminPartnersCtrl($scope, $stateParams, $http) {
+function AdminPartnersCtrl($scope, $stateParams, $state, $http, EmiAuth) {
     $scope.testWord = 'OK';
     //$scope.partnerType = { partner: true, member: true }; //for test only -- actual value to be taken from the partners service
 
@@ -26,9 +26,10 @@ function AdminPartnersCtrl($scope, $stateParams, $http) {
     $scope.showCategories = "partners,members";
     $scope.partnerType = { partner: true, member: true };
 
+    // Filetercategory - Needs to be fixed 
     var partnerTypeInitialEvent = true;
     $scope.$watchCollection('partnerType', function() {
-    	console.log('partnerType is ', $scope.partnerType);
+        console.log('partnerType is ', $scope.partnerType);
         if (!partnerTypeInitialEvent) {
             $scope.showCategories = "";
             angular.forEach($scope.partnerType, function(value, key) {
@@ -44,6 +45,42 @@ function AdminPartnersCtrl($scope, $stateParams, $http) {
         }
         partnerTypeInitialEvent = false;
     });
+
+    // end Filetercategory 
+
+
+    $scope.delete = function(id) {
+        $scope.alerts = [];
+        $http.delete(
+                "/api/partners/delete/" + id,
+                EmiAuth.addAuthHeader({})
+            )
+            .then(response => {
+                $scope.partners = $scope.partners.filter(partners => partners.id != id);
+                $scope.alerts.push({ type: 'success', msg: "Партньорът е успешно изтрит!  &nbsp;&nbsp; <a href='#' class='btn btn-info btn-small' ng-click=\"restore('" + id + "')\">UNDO</a>" });
+            })
+            .catch(err => {
+                $scope.alerts.push({ type: 'danger', msg: "Не е възможно да се изтрие партньорът. Моля опитайте след малко." });
+            })
+    }
+
+    $scope.restore = function(id) {
+        $http.delete(
+                "/api/partners/delete/" + id,
+                EmiAuth.addAuthHeader({ params: { "delete": "false" } })
+            )
+            .then(response => {
+                $state.go('.', {
+                    page: $scope.page,
+                    // published: $scope.published + "",
+                    // showCategories: $scope.showCategories
+                }, { reload: true });
+            })
+            .catch(err => {
+                $scope.alerts.push({ type: 'danger', msg: "Не е възможно да се възтанови изтрития партньор." });
+            })
+    }
+
 
     fetchPartners();
 
