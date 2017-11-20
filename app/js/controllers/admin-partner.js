588b344f1ddc249ffd355d3bc0ee99a7ea41fc4e
mkruneva <marta.kruneva@gmail.com>
2017-11-20 17:19:10 +0000

    single partner and team  - admin ui, no functionality

--- app/js/controllers/admin-partner.js
+++ app/js/controllers/admin-partner.js
@@ -4,7 +4,40 @@ var controllersModule = require('./_index');
 var _ = require('lazy.js');
 
 function AdminPartnerCtrl($scope) {
+    // test
     $scope.testWord = 'AdminPartnerCtrl';
+    // test end
+
+    $scope.alerts = [];
+
+    $scope.cancel = function() {
+        console.log('Cancel ckicked');
+    }
+
+    // var nullify
+
+    $scope.save = function() {
+        console.log('Save clicked');
+    }
+
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
+
+    //// 
+    ////
+    ////
+
+    var init = function(partner) {
+        $scope.partner = partner;
+
+    }
+
+
+
+
 }
 
 controllersModule.controller('AdminPartnerCtrl', AdminPartnerCtrl);
--- app/js/controllers/admin-team.js
+++ app/js/controllers/admin-team.js
@@ -4,7 +4,25 @@ var controllersModule = require('./_index');
 var _ = require('lazy.js');
 
 function AdminTeamCtrl($scope) {
+	// test
     $scope.testWord = 'AdminTeamCtrl';
+    // end Test
+
+    $scope.alerts = [];
+
+    $scope.cancel = function() {
+        console.log('Cancel ckicked');
+    }
+
+    // var nullify
+
+    $scope.save = function() {
+        console.log('Save clicked');
+    }
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
 }
 
 controllersModule.controller('AdminTeamCtrl', AdminTeamCtrl);
