58870b70d3e1ea8bf6a181a51de27187b9dd5d37
mkruneva <marta.kruneva@gmail.com>
2017-11-21 10:46:04 +0000

    team admin - html options for bio input

--- app/js/controllers/admin-team.js
+++ app/js/controllers/admin-team.js
@@ -3,12 +3,11 @@
 var controllersModule = require('./_index');
 var _ = require('lazy.js');
 
-function AdminTeamCtrl($scope, $stateParams, TeamService, ErrorHandling) {
-    // test
-    $scope.testWord = 'AdminTeamCtrl';
-    // end Test
+function AdminTeamCtrl($scope, $rootScope, $stateParams, $sce, TeamService, ErrorHandling) {
 
     $scope.alerts = [];
+    $scope.previousState = $rootScope.previousState;
+    $scope.previousStateParams = $rootScope.previousStateParams;
 
     $scope.cancel = function() {
         console.log('Cancel ckicked');
@@ -24,13 +23,65 @@ function AdminTeamCtrl($scope, $stateParams, TeamService, ErrorHandling) {
         $scope.alerts.splice(index, 1);
     };
 
+
+    //html editor options
+    var editorOptions = function(options, inline, onChange) {
+        options.inline = inline;
+        options.language = $stateParams.lang || 'bg',
+            options.skin = 'lightgray';
+        options.theme = 'modern';
+        options.browser_spellcheck = true;
+        options.init_instance_callback = function(editor) {
+            var textContentTrigger = function() {
+                if (onChange) onChange(editor);
+            };
+
+            editor.on('KeyUp', textContentTrigger);
+            editor.on('ExecCommand', textContentTrigger);
+            editor.on('SetContent', function(e) {
+                if (!e.initial)
+                    textContentTrigger();
+            });
+        };
+        return options;
+    }
+
+    // Bio Html options
+    var htmlOptions = function(updateProperty) {
+        return editorOptions({
+                plugins: [
+                    'google_tools advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen',
+                    'insertdatetime media nonbreaking save table contextmenu directionality',
+                    'template textcolor colorpicker textpattern'
+                ],
+                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link google_tools image | print preview media | forecolor',
+                image_caption: true,
+                image_advtab: true,
+                language: "bg",
+                paste_data_images: true
+            },
+            false
+        );
+    }
+
+    $scope.bioBgOptions = htmlOptions(function(bio) {
+        $scope.team.bio.bg = bio;
+        $scope.bgBio = $sce.trustAsHtml(bio);
+    });
+    $scope.bioEnOptions = htmlOptions(function(bio) {
+        $scope.team.bio.en = bio;
+        $scope.enBio = $sce.trustAsHtml(bio);
+    });
+
+
     var init = function(team) {
         $scope.team = team;
-        
-        // $scope.bgHtml = $sce.trustAsHtml(article.html.bg);
-        // $scope.enHtml = $sce.trustAsHtml(article.html.en);
-        // var html = angular.copy(article.html);
-        // $scope.html = html;
+
+        $scope.bgBio = $sce.trustAsHtml(team.bio.bg);
+        $scope.enBio = $sce.trustAsHtml(team.bio.en);
+        var bio = angular.copy(team.bio);
+        $scope.bio = bio;
     }
 
     if ($stateParams.id) {
