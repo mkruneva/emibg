cd8bcaa05294d6b199d2d8880345b410ac0c166e
mkruneva <marta.kruneva@gmail.com>
2017-11-30 17:19:57 +0000

    admin partners save and cancel working

--- app/js/controllers/admin-partner.js
+++ app/js/controllers/admin-partner.js
@@ -3,7 +3,7 @@
 var controllersModule = require('./_index');
 var _ = require('lazy.js');
 
-function AdminPartnerCtrl($scope, $stateParams, PartnerService) {
+function AdminPartnerCtrl($scope, $rootScope, $state, $stateParams, $http, $sce, PartnerService, EmiAuth, ErrorHandling) {
     // test
     $scope.testWord = 'AdminPartnerCtrl';
     // test end
@@ -12,12 +12,35 @@ function AdminPartnerCtrl($scope, $stateParams, PartnerService) {
 
     $scope.cancel = function() {
         console.log('Cancel ckicked');
+        if ($rootScope.previousState) {
+            $state.go($scope.previousState, $scope.previousStateParams);
+        } else {
+            $state.go("app.admin.partners");
+        }
     }
 
     // var nullify
 
     $scope.save = function() {
-        console.log('Save clicked');
+
+        $scope.alerts = [];
+
+        var method = $http.post;
+        var url = "/api/partners";
+
+        if ($scope.partner.id) {
+            method = $http.put;
+            url = "/api/partners/" + $scope.partner.id;
+        }
+        ErrorHandling.handle(method(url, $scope.partner, EmiAuth.addAuthHeader({})))
+            .then(function(data) {
+                $scope.partner = data;
+                $scope.alerts.push({ type: 'success', msg: $sce.trustAsHtml("Партньорът е записан успешно") });
+            })
+            .catch(function(err) {
+                $scope.alerts.push({ type: 'danger', msg: "Не е възможно да се запише партньорът в момента. Моля опитайте отново. " + err });
+                console.log(err);
+            });
     }
 
 
@@ -34,8 +57,7 @@ function AdminPartnerCtrl($scope, $stateParams, PartnerService) {
         PartnerService.partner($stateParams.id)
             .then(init)
             .catch(err => $scope.alerts.push({ type: 'danger', msg: err + "" }));
-    } 
-    else {
+    } else {
         init({
             title: { bg: '', en: '' },
             localId: '',
