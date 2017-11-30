57de6684727fc49f48b3be7184486518afc9bdbb
mkruneva <marta.kruneva@gmail.com>
2017-11-30 16:09:53 +0000

    partners - filtered by localId and type

--- app/js/controllers/all-partners.js
+++ app/js/controllers/all-partners.js
@@ -6,12 +6,14 @@ var controllersModule = require('./_index');
 * @ngInject
 */
 
-function AllPartnersCtrl($scope, PartnerService) {                                                                                                  
+function AllPartnersCtrl($scope, $filter, PartnerService) {                                                                                                  
 	$scope.allPartners = PartnerService.allPartners()
 	.then(partners => {
 		$scope.allPartners = partners;
-		$scope.members = partners.filter(partner => partner.type == "member");
-		$scope.partners = partners.filter(partner => partner.type == "partner")
+		var members = partners.filter(partner => partner.type == "member");
+		$scope.members = $filter('orderBy')(members, 'localId');
+		var partners = partners.filter(partner => partner.type == "partner")
+		$scope.partners = $filter('orderBy')(partners, 'localId');
 	});
 }
 
