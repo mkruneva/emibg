3c4d36732d01332f38e944ffff7fbb97b0015807
mkruneva <marta.kruneva@gmail.com>
2017-12-07 16:41:08 +0000

    team bio show only when bio en or bio bg exists

--- app/js/controllers/all-teams.js
+++ app/js/controllers/all-teams.js
@@ -6,7 +6,7 @@ function AllTeamsCtrl($scope, $sce, $filter, TeamService) {
     $scope.allTeams = TeamService.allTeams()
         .then(teams => {
             teams = teams.map(elem => {
-            	if (elem.bio) {elem.hasBio = true} else {elem.hasBio = false};
+            	if (elem.bio.bg || elem.bio.en) {elem.hasBio = true} else {elem.hasBio = false};
                 elem.bio = $sce.trustAsHtml($filter('lang')(elem.bio));
                 return elem;
             });
