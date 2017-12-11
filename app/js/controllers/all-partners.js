fb7dde45c786358fe3ce0544a3927af5a8220cc2
mkruneva <marta.kruneva@gmail.com>
2017-12-11 20:43:21 +0000

    loader for partners

--- app/js/controllers/all-partners.js
+++ app/js/controllers/all-partners.js
@@ -8,6 +8,7 @@ var controllersModule = require('./_index');
 
 function AllPartnersCtrl($scope, $filter, PartnerService) {
     $scope.alerts = [];
+    $scope.loading = true;
     $scope.allPartners = PartnerService.allPartners()
         .then(partners => {
             $scope.allPartners = partners;
@@ -15,10 +16,12 @@ function AllPartnersCtrl($scope, $filter, PartnerService) {
             $scope.members = $filter('orderBy')(members, 'localId');
             var partners = partners.filter(partner => partner.type == "partner")
             $scope.partners = $filter('orderBy')(partners, 'localId');
+            $scope.loading = false;
         })
         .catch(err => {
             $scope.alerts.push({ type: 'danger', msg: "Ами сега!? Възникнала е грешка по при комуникацията със сървъра. Моля опитайте отново по-късно. " + err });
             console.log("APP ERROR: " + err);
+            $scope.loading = false;
         });
 }
 
